ca59abd440de2c0ddac89cc2f1469384c40083ce
Chang Guo <changguo1998@gmail.com>
2025-08-12 14:38:10 +0800

    archive old codes

--- src/FMTools.jl
+++ src/FMTools.jl
@@ -1,19 +1,6 @@
 module FMTools
 
-using Dates, DelimitedFiles, LinearAlgebra, Printf, SeisTools, Statistics, TOML, SHA
+using Dates, DelimitedFiles, LinearAlgebra, Printf, Statistics, TOML, SHA
 
-SSAC = SeisTools.SAC
-SDP = SeisTools.DataProcess
-SSR = SeisTools.Source
-SGD = SeisTools.Geodesy
-
-# DEBUG = true
-
-include("macros.jl")
-include("sourcetimefunction.jl")
-include("types.jl")
-include("phasereport.jl")
-include("io.jl")
-include("calc.jl")
 
 end # module
